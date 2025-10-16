47cee1c47cd44d43687a9a7ce9a2369f959b2c0f
Smitali Bhandari <ce24b119@smail.iitm.ac.in>
2025-10-17 00:58:53 +0530

    added lin reg +visualization for 3state- 2d simplex using this

--- toy_transformer/ICL/toy_model.py
+++ toy_transformer/ICL/toy_model.py
@@ -2,23 +2,141 @@
 import os
 
 import numpy as np
-
+import pandas as pd
+import datashader as ds
+import datashader.transfer_functions as tf
 import torch
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader, Subset
 from tqdm import tqdm
 import wandb
-from typing import Optional, Literal
+from matplotlib import pyplot as plt
+from PIL import Image
+from matplotlib.figure import Figure
 
+from jaxtyping import Float
 from transformer_lens import HookedTransformer, HookedTransformerConfig
 from transformer_lens.train import HookedTransformerTrainConfig
 from metrics import MetricsConfig, generate_prefix_matching_data, compute_metrics
 from mealymarkov import MarkovMealyModel
-import numpy as np
-from typing import List, Optional, Tuple
-import numpy as np
 from typing import List, Optional, Tuple
 import scipy
+from sklearn.linear_model import LinearRegression
+
+def run_activation_to_beliefs_regression(activations, ground_truth_beliefs):
+
+    # make sure the first two dimensions are the same
+    assert activations.shape[0] == ground_truth_beliefs.shape[0]
+    assert activations.shape[1] == ground_truth_beliefs.shape[1]
+
+    # flatten the activations
+    batch_size, n_ctx, d_model = activations.shape
+    belief_dim = ground_truth_beliefs.shape[-1]
+    activations_flattened = activations.reshape(-1, d_model)  # [batch * n_ctx, d_model]
+    ground_truth_beliefs_flattened = ground_truth_beliefs.reshape(
+        -1, belief_dim
+    )  # [batch * n_ctx, belief_dim]
+
+    # run the regression
+    regression = LinearRegression()
+    regression.fit(activations_flattened, ground_truth_beliefs_flattened)
+
+    # get the belief predictions
+    belief_predictions = regression.predict(
+        activations_flattened
+    )  # [batch * n_ctx, belief_dim]
+    belief_predictions = belief_predictions.reshape(batch_size, n_ctx, belief_dim)
+
+    return regression, belief_predictions
+
+
+def _project_to_simplex(points:np.ndarray):
+    """Project points onto the 2-simplex (equilateral triangle in 2D)."""
+    x = points[:, 1] + 0.5 * points[:, 2]
+    y = (np.sqrt(3) / 2) * points[:, 2]
+    return x, y
+
+# Combine aggregated channels into RGB images
+def _combine_channels_to_rgb(agg_r, agg_g, agg_b, px:int):
+    img_r = tf.shade(agg_r, cmap=['black', 'red'], how='linear')
+    img_g = tf.shade(agg_g, cmap=['black', 'green'], how='linear')
+    img_b = tf.shade(agg_b, cmap=['black', 'blue'], how='linear')
+
+    img_r = tf.spread(img_r, px=px, shape='circle')
+    img_g = tf.spread(img_g, px=px, shape='circle')
+    img_b = tf.spread(img_b, px=px, shape='circle')
+
+    # Combine using numpyß
+    r_array = np.array(img_r.to_pil()).astype(np.float64)
+    g_array = np.array(img_g.to_pil()).astype(np.float64)
+    b_array = np.array(img_b.to_pil()).astype(np.float64)
+
+    
+    # Stack arrays into an RGB image (ignoring alpha channel for simplicity)
+    rgb_image = np.stack([r_array[:,:,0], g_array[:,:,1], b_array[:,:,2]], axis=-1)
+    
+    return Image.fromarray(np.uint8(rgb_image))
+
+# TODO: I changed up the code for this to something which makes sense to me (creating panda dataframes from ground truth and predicted simplex. Check to see if this is what should actually be done)
+def plot_ground_truth_and_evaluated_2d_simplex(
+    ground_truth_tensor: np.ndarray, 
+    predicted_beliefs: np.ndarray, 
+    plot_triangles: bool,
+    facecolor: Literal['black', 'white'],
+    px: int
+) -> Figure:
+    # Projection and DataFrame preparation
+    ground_truth_tensor=ground_truth_tensor.reshape(-1,ground_truth_tensor.shape[-1])
+    print(ground_truth_tensor.shape)
+    bs_x, bs_y = _project_to_simplex(np.array(ground_truth_tensor))
+    bs_x, bs_y = np.ravel(bs_x), np.ravel(bs_y)
+    print("bs_x shape:", np.shape(bs_x))
+    print("bs_y shape:", np.shape(bs_y))
+    print("ground_truth_tensor shape:", np.shape(ground_truth_tensor))
+   #ground_truth_tensor=ground_truth_tensor.reshape(-1,ground_truth_tensor.shape[-1])
+    ground_truth_data_frame = pd.DataFrame({'x': bs_x, 'y': bs_y, 'r': ground_truth_tensor[:, 0], 'g': ground_truth_tensor[:, 1], 'b': ground_truth_tensor[:, 2]})
+    predicted_beliefs=predicted_beliefs.reshape(-1,predicted_beliefs.shape[-1])
+    print(predicted_beliefs.shape)
+    pb_x, pb_y = _project_to_simplex(np.array(predicted_beliefs))
+    pb_x, pb_y = np.ravel(pb_x), np.ravel(pb_y)
+    print("pb_x shape:", np.shape(pb_x))
+    print("pb_y shape:", np.shape(pb_y))
+    print("predicted_belief shape:", np.shape(predicted_beliefs))
+    predicted_belief_vector_data_frame = pd.DataFrame({'x': pb_x, 'y': pb_y, 'r': ground_truth_tensor[:, 0], 'g': ground_truth_tensor[:, 1], 'b': ground_truth_tensor[:, 2]})
+
+    # Create canvas
+    canvas = ds.Canvas(plot_width=1000, plot_height=1000, x_range=(-0.1, 1.1), y_range=(-0.1, np.sqrt(3)/2 + 0.1))
+    
+    # Aggregate each RGB channel separately for ground truth and predicted beliefs
+    colours = ['r', 'g', 'b']
+    ground_truth_aggregated = {color: canvas.points(ground_truth_data_frame, 'x', 'y', ds.mean(color)) for color in colours}
+    predicted_belief_vector_aggregated = {color: canvas.points(predicted_belief_vector_data_frame, 'x', 'y', ds.mean(color)) for color in colours}
+
+    img_gt = _combine_channels_to_rgb(ground_truth_aggregated['r'], ground_truth_aggregated['g'], ground_truth_aggregated['b'], px=2*px)
+    img_pb = _combine_channels_to_rgb(predicted_belief_vector_aggregated['r'], predicted_belief_vector_aggregated['g'], predicted_belief_vector_aggregated['b'], px=px)
+
+    # Visualization with Matplotlib
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5), sharex=True, sharey=True, facecolor=facecolor)
+    for ax in axs:
+        ax.tick_params(axis='x', colors=facecolor)  
+        ax.tick_params(axis='y', colors=facecolor)  
+        ax.xaxis.label.set_color(facecolor)  
+        ax.yaxis.label.set_color(facecolor)  
+        ax.title.set_color(facecolor)
+    axs[0].imshow(img_gt)
+    axs[1].imshow(img_pb)
+    
+    axs[0].axis('off')
+    axs[1].axis('off')
+    title_y_position = -0.1  # Adjust this value to move the title up or down relative to the axes
+    fig.text(0.5, title_y_position, 'Ground Truth', ha='center', va='top', transform=axs[0].transAxes, color='white', fontsize=15)  # Changed 'black' to 'white'
+    fig.text(0.5, title_y_position, 'Residual Stream', ha='center', va='top', transform=axs[1].transAxes, color='white', fontsize=15)  # Changed 'black' to 'white'
+
+    if plot_triangles:
+        for ax in axs:
+            ax.plot([0, 0.5, 1, 0], [0, np.sqrt(3)/2, 0, 0], 'white', lw=2)  # Changed 'black' to 'white'
+
+    return fig    
 
 class ParallelMarkovGeneratorGPU:
     """
